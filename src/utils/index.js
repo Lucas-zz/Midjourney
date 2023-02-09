656432a4c639230bb8b3c727f9aebf400a7ff618
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-02-08 22:36:45 -0300

    chore: getRandomPrompt and downloadImage utils done

---
+++ src/utils/index.js
@@ -0,0 +1,16 @@
+import FileSaver from 'file-saver';
+
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+};
+
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+};
