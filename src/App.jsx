2d5c8fbf3026c024a4651cb9c975d1134c640158
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-02-08 22:41:51 -0300

    feat: app structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#E6EBF4]">
+        <Link to="/">
+          <img src={logo} alt={logo} className="w-28 object-contain" />
+        </Link>
+        <Link to="/create-post" className="font-inter font-medium bg-[#6469FF] text-white px-4 py-2 rounded-md">
+          Create
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#F9F9F9] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
